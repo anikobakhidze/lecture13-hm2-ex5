35bec99b9a8477c7f3e1c2813df41aa0c752770e
anikobakhidze <80447919+anikobakhidze@users.noreply.github.com>
2022-12-24 14:05:54 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let str='Hello World ';
-let strEdit=str.split(' ').join('');
-let arr=Array.from(strEdit.split(''));
-console.log(arr);
-let result=arr.filter(el => el===el.toUpperCase());
-console.log(result.join(''));
+let str = "Hello World";
+let abb = str
+  .split(" ")
+  .map((el) => el.charAt(0))
+  .join("")
+  .toUpperCase();
+console.log(abb);
